44414e1c46605c0a9e166877530ac354ed2a294b
alessandro-fior <73718633+alessandro-fior@users.noreply.github.com>
2021-06-24 14:57:04 +0200

    vedi corso
    vedi corso

---
+++ asp28/CorsoAuth/CorsoAuth/Models/UtenteViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CorsoAuth.Models
+{
+    public class UtenteLoginModel
+    {
+        [Required]
+        [EmailAddress]
+        [StringLength(150, ErrorMessage = "Email troppo lunga (Max 150 caratteri).")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(20, MinimumLength = 6)]
+        public string Password { get; set; }
+    }
+
+    public class UtenteCreateModel
+    {
+        [Required]
+        [EmailAddress]
+        [StringLength(150, ErrorMessage = "Email troppo lunga (Max 150 caratteri).")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Username troppo lungo (Max 50 caratteri).")]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(20, MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(20, MinimumLength = 6)]
+        [Display(Name = "Conferma Password")]
+        [Compare("Password")]
+        public string RePassword { get; set; }
+    }
+
+}
