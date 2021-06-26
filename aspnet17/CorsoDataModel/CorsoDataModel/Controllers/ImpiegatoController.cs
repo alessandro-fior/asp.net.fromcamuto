ff7902a55d8b836c30b924b5376a434bd8eeb4a1
alessandro-fior <73718633+alessandro-fior@users.noreply.github.com>
2021-06-26 19:27:11 +0200

    x
    x

---
+++ aspnet17/CorsoDataModel/CorsoDataModel/Controllers/ImpiegatoController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CorsoDataModel.Models;
+
+namespace CorsoDataModel.Controllers
+{
+    public class ImpiegatoController : Controller
+    {
+        //private Database1Entities db = new Database1Entities();
+
+        //
+        // GET: /Impiegato/
+        public ActionResult Index()
+        {
+          List<Impiegato> model = new List<Impiegato>(); 
+          using (Database1Entities db = new Database1Entities())
+          {
+            model = db.Impiegati.ToList();
+          }
+         
+          return View(model);
+
+            //return View( db.Impiegati.ToList() );
+        }
+	}
+}
